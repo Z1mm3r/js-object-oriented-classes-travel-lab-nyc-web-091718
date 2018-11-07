05bb5ea985129e50921a67194761f6f596deccae
Z1mm3r <czimmer@ramapo.edu>
2018-11-07 09:01:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+
+class Driver{
+  constructor(name,startDate){
+    this.name = name
+    this.startDate = fakeToRealDate(startDate);
+  }
+
+  startDate(){
+    return startDate
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+
+}
+
+class Route{
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let distance = 0
+    let startAve = eastWest.indexOf(this.beginningLocation["horizontal"])
+    let endAve = eastWest.indexOf(this.endingLocation["horizontal"])
+
+    distance += Math.abs(startAve - endAve);
+    distance += Math.abs(parseInt(this.beginningLocation["vertical"]) - parseInt(this.endingLocation["vertical"]))
+    return distance
+  }
+
+  estimatedTime(isPeak){
+    if(isPeak)
+      return this.blocksTravelled() / 2
+    return this.blocksTravelled() / 3
+  }
+
+}
+
+function fakeToRealDate(fakeDate)
+{
+  return new Date(`${fakeDate} 00:00:00`);
+}
